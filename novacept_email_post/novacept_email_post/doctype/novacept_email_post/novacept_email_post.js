03d7e38a1646313ad3abf2ee83a13b48f9e7390c
HarshMagiya7 <harshmagiya07@gmail.com>
2022-12-23 19:54:15 +0000

    first commit

---
+++ novacept_email_post/novacept_email_post/doctype/novacept_email_post/novacept_email_post.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Novacept Limited and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Novacept Email Post', {
+	// refresh: function(frm) {
+
+	// }
+});
